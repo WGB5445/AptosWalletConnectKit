f786608eec55e12bd46b4627cded7f1c87e92db4
WGB5445 <919603023@qq.com>
2025-06-16 21:05:38 +0800

    feat: enhance WalletAdapter with additional wallet features and error handling

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as Modal from './WalletConnectModal';
 import * as Button from './WalletConnectButton';
 import * as Context from './Context';
 import * as WalletInfoModal from './WalletInfoModal';
-import { AptosDisconnectNamespace, AptosGetAccountNamespace, type AptoGetsAccountOutput, type AptosConnectFeature, type AptosDisconnectFeature, type AptosGetAccountFeature } from '@aptos-labs/wallet-standard';
+import { AptosDisconnectNamespace, AptosGetAccountNamespace, AptosGetNetworkNamespace, AptosSignAndSubmitTransactionNamespace, AptosSignMessageNamespace, AptosSignTransactionNamespace, type AptoGetsAccountOutput, type AptosConnectFeature, type AptosDisconnectFeature, type AptosGetAccountFeature, type AptosGetNetworkFeature, type AptosSignAndSubmitTransactionFeature, type AptosSignAndSubmitTransactionInput, type AptosSignAndSubmitTransactionOutput, type AptosSignMessageFeature, type AptosSignMessageInput, type AptosSignMessageOutput, type AptosSignTransactionFeature, type AptosSignTransactionInputV1_1, type AptosSignTransactionOutput, type AptosSignTransactionOutputV1_1, type NetworkInfo, type UserResponse } from '@aptos-labs/wallet-standard';
 
 class WalletAdapter {
   private store;
@@ -13,6 +13,10 @@ class WalletAdapter {
 //   connect() {
 //     (this.store.wallet?.features as AptosConnectFeature)[AptosConnectNamespace].connect()
 //   }
+
+  isConnected() {
+    return this.store.wallet !== undefined && this.store.address !== undefined;
+  }
 
   disconnect() {
     if (!this.store.wallet) {
@@ -27,6 +31,57 @@ class WalletAdapter {
     }
     return (this.store.wallet.features as AptosGetAccountFeature)[AptosGetAccountNamespace].account()
   }
+
+  getNetwokrInfo(): Promise<NetworkInfo> {
+    if (!this.store.wallet) {
+      throw new Error('No wallet connected');
+    }
+    return (this.store.wallet.features as AptosGetNetworkFeature)[AptosGetNetworkNamespace].network();
+  }
+
+  async getAccount(): Promise<string> {
+    if (!this.store.wallet) {
+        throw new Error('No wallet connected');
+
+    }
+    return (this.store.wallet.features as AptosGetAccountFeature)[AptosGetAccountNamespace].account().then((v)=> v.address.toString());
+  }
+
+  signMessage(message: AptosSignMessageInput): Promise<UserResponse<AptosSignMessageOutput>> {
+    if (!this.store.wallet) {
+      throw new Error('No wallet connected');
+    }
+    return (this.store.wallet.features as AptosSignMessageFeature)[AptosSignMessageNamespace].signMessage(message)
+  }
+
+  signAndSubmitTransaction(
+    transaction: AptosSignAndSubmitTransactionInput,
+  ): Promise<UserResponse<AptosSignAndSubmitTransactionOutput>> {
+    if (!this.store.wallet) {
+      throw new Error('No wallet connected');
+    }
+    return (this.store.wallet.features as AptosSignAndSubmitTransactionFeature)[AptosSignAndSubmitTransactionNamespace].signAndSubmitTransaction(transaction);
+  }
+
+  signTransaction(
+    transaction: AptosSignTransactionInputV1_1,
+  ): Promise<UserResponse<AptosSignTransactionOutputV1_1>> {
+    if (!this.store.wallet) {
+      throw new Error('No wallet connected');
+    }
+    const feature = (this.store.wallet.features as Record<string, any>)[AptosSignTransactionNamespace] as AptosSignTransactionFeature['aptos:signTransaction'];
+    if (!feature || typeof feature.version !== 'string') {
+      throw new Error('signTransaction feature is not available or has no version');
+    }
+    if (feature.version === '1.0.0') {
+      throw new Error('Wallet does not support signTransaction feature');
+    }
+    if (feature.version !== '1.1') {
+      throw new Error(`Unsupported version of signTransaction feature: ${feature['version']}`);
+    }
+    return feature.signTransaction(transaction);
+  }
+
 }
 
 const walletAdapter = new WalletAdapter();
